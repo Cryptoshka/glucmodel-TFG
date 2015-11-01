365a0427a16a2f0389b6ec65e7fa6176f3054053
Alberto Miedes Garcés <albertomg994@gmail.com>
2015-11-01 16:06:42 +0100

    Lista de recomendaciones implementada con AngularJS
    - Carga la lista desde un JSON de ejemplo
    - He puesto un buscador
    - He puesto un selector para ordenar en función varios criterios

--- WebContent/js/controladoresMedico.js
+++ WebContent/js/controladoresMedico.js
@@ -1,13 +1,21 @@
 'use strict';
 
-/* Controlador de la lista de pacientes */
+/* Controlador para el médico */
 
 var appMedico = angular.module('appMedico', []);
 
-appMedico.controller('PatientListCtrl', ['$scope', '$http', function($scope, $http) {
-  $http.get('json_examples/lista_pacientes.json').success(function(data) {
-    $scope.pacientes = data;
-  });
+appMedico.controller('ControladorMedico', ['$scope', '$http', function($scope, $http) {
+	
+	/* Recuperación de la lista de pacientes */
+	$http.get('json_examples/lista_pacientes.json').success(function(data) {
+		$scope.pacientes = data;
+	});
+	
+	/* Recuperación de la lista de recomendaciones sugeridas */
+	$http.get('json_examples/lista_recs_sugeridas.json').success(function(data) {
+		$scope.lista_recs = data;
+	});
 
-  $scope.orderProp = 'age';
+	$scope.ordenRecs = 'date';
+  
 }]);
